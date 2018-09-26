57f1f8744842227eb39fcd190d18dd3f60de4204
oceanaut <mavieksen@gmail.com>
2018-09-26 09:21:35 +0300

    day 05 added

---
+++ hackerrank30days/day05/Solution.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+#https://www.hackerrank.com/challenges/30-loops/problem
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    if n in range(2,21):
+        for i in range(1,11):
+            print(str(n) + " x " + str(i) + " = " + str(i*n) )
