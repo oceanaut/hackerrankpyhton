504fe67483d9a1008f05921a75a02c9df907e73a
oceanaut <mavieksen@gmail.com>
2018-09-30 14:46:57 +0300

    day 08 added

---
+++ hackerrank30days/day09/Solution.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the factorial function below.
+def factorial(n):
+    if n>1: return n*factorial(n-1)
+    else: return n
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+    if n in range (2,13): result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
