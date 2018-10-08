e4c40c3f94be7d6b91cd83a4e6b9fe04e3f301d4
oceanaut <mavieksen@gmail.com>
2018-10-08 13:54:32 +0300

    Find the Runner-Up Score!

---
+++ basic-data-types/Runner-Up-Score.py
@@ -0,0 +1,8 @@
+#https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
+
+#!/bin/python3
+
+if __name__ == '__main__':
+    n= int(input())
+    arr = map(int, input().split())
+    print( sorted(list(set(arr)))[-2] )
