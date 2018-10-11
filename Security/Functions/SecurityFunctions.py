4d2abbaabde93e7f6368fd7223c2586eed70c9d1
oceanaut <mavieksen@gmail.com>
2018-10-11 12:24:28 +0300

    Security Functions

---
+++ Security/Functions/SecurityFunctions.py
@@ -0,0 +1,23 @@
+#https://www.hackerrank.com/challenges/security-tutorial-functions/problem
+
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the calculate function below.
+def calculate(x):
+    return x%11
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    x = int(input())
+
+    result = calculate(x)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
