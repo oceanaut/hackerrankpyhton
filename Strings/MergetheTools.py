60cd7faf6e54323f55f58c8677485eb4a534295d
oceanaut <mavieksen@gmail.com>
2018-11-20 23:54:22 +0300

    Merge the Tools!

---
+++ Strings/MergetheTools.py
@@ -0,0 +1,18 @@
+# https://www.hackerrank.com/challenges/merge-the-tools/problem
+
+#!/bin/python3
+
+
+
+def merge_the_tools(string, k):
+    for part in zip(*[iter(string)] * k):
+        d = dict()
+        print(''.join([ d.setdefault(c, c) for c in part if c not in d ]))
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
+
+
+
+
