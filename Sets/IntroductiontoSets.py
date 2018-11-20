3db4922b923ee7beabe16309145cdf80dcad6a1d
oceanaut <mavieksen@gmail.com>
2018-11-21 00:02:38 +0300

    Introduction to Sets

---
+++ Sets/IntroductiontoSets.py
@@ -0,0 +1,14 @@
+# https://www.hackerrank.com/challenges/py-introduction-to-sets/problem
+
+#!/bin/python3
+
+def average(array):
+    return sum(set(array))/len(set(array))
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
+
+
