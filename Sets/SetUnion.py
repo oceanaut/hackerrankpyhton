6a8a07a7533e6ef27ec6d08739edbbf6cf664d77
oceanaut <mavieksen@gmail.com>
2019-01-22 00:54:57 +0300

    SetUnion

---
+++ Sets/SetUnion.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/py-set-union/problem
+
+#!/bin/python3
+
+if __name__ == '__main__':
+    x, a, y, b = [set(input().split()) for _ in '1234'];print(len(a | b))
+
