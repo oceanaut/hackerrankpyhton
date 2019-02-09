2c8725f9419b65482c529ee39471f9557aec3e89
oceanaut <mavieksen@gmail.com>
2019-02-09 23:11:20 +0300

    XML

---
+++ XML/XML1.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+"""
+https://www.hackerrank.com/challenges/xml-1-find-the-score/problem
+You are given a valid XML document, and you have to print its score.
+The score is calculated by the sum of the score of each element.
+For any element, the score is equal to the number of attributes it has.
+"""
+
+import sys
+import xml.etree.ElementTree as etree
+
+def traverse(node):
+  return len(node.attrib) + sum(traverse(child) for child in node)
+
+
+def get_attr_number(node):
+    # your code goes here
+    return traverse(node)
+
+
+if __name__ == '__main__':
+    sys.stdin.readline()
+    xml = sys.stdin.read()
+    tree = etree.ElementTree(etree.fromstring(xml))
+    root = tree.getroot()
+    print(get_attr_number(root))
